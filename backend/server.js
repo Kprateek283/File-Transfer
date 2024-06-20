25811a88d0eae6c2b20349331c3cabea4875c66e
Prateek <kprateek283@gmail.com>
2024-06-20 18:07:47 +0530

    added backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import router from './routes/routes.js';
+import DBConnection from './database/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+// Configure CORS to allow requests only from http://localhost:3000
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/', router);
+
+const PORT = process.env.PORT || 8000;
+
+DBConnection();
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
